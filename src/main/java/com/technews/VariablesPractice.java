b1eacef20a8b5c48459105e115c43b966fa77a6b
jthnguyen9909 <jthnguyen9909@gmail.com>
2023-05-04 13:51:10 -0700

    created user, post, comment, vote models

---
+++ src/main/java/com/technews/VariablesPractice.java
@@ -0,0 +1,17 @@
+package com.technews;
+
+public class VariablesPractice {
+    public static void main(String[] args) {
+        int testInt = 2;
+        double testDouble = 4.99;
+        double sum = testInt + testDouble;
+
+
+        String testStr = "dog";
+        String test = testStr + testInt;
+
+        System.out.println(test);
+
+
+    }
+}
